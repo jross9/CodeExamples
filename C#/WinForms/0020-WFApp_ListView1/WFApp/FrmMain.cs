2bd263a4c0f5ad9cc41443f43af09e7430086bf1
jross9 <ross.ja@gmail.com>
2023-09-24 22:36:36 -0600

    0020-WFApp_ListView1

---
+++ C#/WinForms/0020-WFApp_ListView1/WFApp/FrmMain.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WFApp
+{
+    public partial class FrmMain : Form
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+
+            this.lvItems.Items.Add("Item 1");
+            this.lvItems.Items.Add("Item 2");
+            this.lvItems.Items.Add("Item 3");
+            this.lvItems.Items.Add("Item 4");
+            this.lvItems.Items.Add("Item 5");
+            this.lvItems.Items.Add("Item 6");
+            this.lvItems.Items.Add("Item 7");
+        }
+    }
+}
