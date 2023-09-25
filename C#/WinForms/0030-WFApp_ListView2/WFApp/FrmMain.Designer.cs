cdfb96733cfa474fdad3d3fb8acdd434b2ae35d6
jross9 <ross.ja@gmail.com>
2023-09-24 22:43:59 -0600

    0030-WFApp_ListView2

---
+++ C#/WinForms/0030-WFApp_ListView2/WFApp/FrmMain.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace WFApp
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lvItems = new System.Windows.Forms.ListView();
+            this.colHeader = new System.Windows.Forms.ColumnHeader();
+            this.SuspendLayout();
+            // 
+            // lvItems
+            // 
+            this.lvItems.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.colHeader});
+            this.lvItems.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lvItems.FullRowSelect = true;
+            this.lvItems.GridLines = true;
+            this.lvItems.Location = new System.Drawing.Point(0, 0);
+            this.lvItems.Name = "lvItems";
+            this.lvItems.Size = new System.Drawing.Size(280, 260);
+            this.lvItems.TabIndex = 0;
+            this.lvItems.UseCompatibleStateImageBehavior = false;
+            this.lvItems.View = System.Windows.Forms.View.Details;
+            // 
+            // colHeader
+            // 
+            this.colHeader.Text = "Items";
+            this.colHeader.Width = 245;
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(280, 260);
+            this.Controls.Add(this.lvItems);
+            this.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "FrmMain";
+            this.Text = "Items Apps";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView lvItems;
+        private System.Windows.Forms.ColumnHeader colHeader;
+    }
+}
+
---
+++ C#/WinForms/0030-WFApp_ListView2/WFApp/FrmMain.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WFApp
+{
+    public partial class FrmMain : Form
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+
+            for (int i = 1; i < 50; i++)
+            {
+                this.lvItems.Items.Add("Item " + i);
+            }
+        }
+
+		protected override void OnLoad(EventArgs e)
+		{
+			base.OnLoad(e);
+
+            this.lvItems.Columns[0].Width = lvItems.Width - SystemInformation.VerticalScrollBarWidth - 5;
+        }
+
+		protected override void OnResize(EventArgs e)
+		{
+			base.OnResize(e);
+
+			this.lvItems.Columns[0].Width = lvItems.Width - SystemInformation.VerticalScrollBarWidth - 5;
+		}
+	}
+}
