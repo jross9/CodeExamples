57983b625898b129b59f6b5606a6513054dbf85e
jross9 <ross.ja@gmail.com>
2023-09-26 17:46:22 -0600

    0040-WFApp_ListView3

---
+++ C#/WinForms/0040-WFApp_ListView3/WFApp/FrmMain.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace WFApp
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lvItems = new System.Windows.Forms.ListView();
+            this.colHeader = new System.Windows.Forms.ColumnHeader();
+            this.lblMsg = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lvItems
+            // 
+            this.lvItems.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.colHeader});
+            this.lvItems.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lvItems.FullRowSelect = true;
+            this.lvItems.GridLines = true;
+            this.lvItems.HideSelection = false;
+            this.lvItems.Location = new System.Drawing.Point(0, 0);
+            this.lvItems.MultiSelect = false;
+            this.lvItems.Name = "lvItems";
+            this.lvItems.Size = new System.Drawing.Size(280, 224);
+            this.lvItems.TabIndex = 0;
+            this.lvItems.UseCompatibleStateImageBehavior = false;
+            this.lvItems.View = System.Windows.Forms.View.Details;
+            this.lvItems.SelectedIndexChanged += new System.EventHandler(this.lvItems_SelectedIndexChanged);
+            // 
+            // colHeader
+            // 
+            this.colHeader.Text = "Items";
+            this.colHeader.Width = 245;
+            // 
+            // lblMsg
+            // 
+            this.lblMsg.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.lblMsg.Location = new System.Drawing.Point(0, 237);
+            this.lblMsg.Name = "lblMsg";
+            this.lblMsg.Size = new System.Drawing.Size(280, 23);
+            this.lblMsg.TabIndex = 1;
+            this.lblMsg.Text = "label1";
+            this.lblMsg.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(280, 260);
+            this.Controls.Add(this.lblMsg);
+            this.Controls.Add(this.lvItems);
+            this.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "FrmMain";
+            this.Text = "Items Apps";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView lvItems;
+        private System.Windows.Forms.ColumnHeader colHeader;
+        private System.Windows.Forms.Label lblMsg;
+    }
+}
+
