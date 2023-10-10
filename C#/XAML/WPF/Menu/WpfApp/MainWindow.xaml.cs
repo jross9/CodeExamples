98b9ea2aca7c1786dca33c49bda7a84f16fe97a4
jross9 <ross.ja@gmail.com>
2023-10-09 23:08:54 -0600

    sync w/ local

--- C#/XAML/WPF/Menu/WpfApp/MainWindow.xaml.cs
+++ C#/XAML/WPF/Menu/WpfApp/MainWindow.xaml.cs
@@ -1,8 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -13,7 +12,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace WpfApp
+namespace WpfApp 
 {
 	/// <summary>
 	/// Interaction logic for MainWindow.xaml
